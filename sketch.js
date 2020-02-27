8734d9c0e5e15eda738564f92bd5227e2cdb0618
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-02-26 20:24:57 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
-//myFeelings
-let feelings = [ "I'm lost", "I feel alone", "Not sure who I am supposed to be", "What's the point of anything?"];
+//Feelings
+let feelings = [{
+  10 am: "I'm lost"
+}, {
+   2 pm: "I feel alone"
+}, {
+   7 pm: "Not sure who I am supposed to be"
+}, {
+   12 am: "What's the point of anything?"
+}, {
+   2 am: "I'm tired"
+}];
 
 function setup() {
   createCanvas(600, 600);
 
-  console.log(dogs[2]);
+  console.log(feelings[0]);
 
 }
 
