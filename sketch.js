f4d2d6172cad0a77df6a8bb4e153f8e2b330c286
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-02-26 20:06:08 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+//myFeelings
+let feelings = [ "I'm lost", "I feel alone", "Not sure who I am supposed to be", "What's the point of anything?"];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  console.log(dogs[2]);
+
 }
 
 function draw() {
