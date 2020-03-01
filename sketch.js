6d7040daad5674c1b8479d79c2744b5fd48d9b2e
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-02-29 16:21:10 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,37 +17,61 @@ let feelings = [{
 }];
 
 let randomIndex;
+let animating = false;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
   textSize(32);
 
-  text("Hm", 250, 300);
+  text("Start", 250, 300);
+
+  setTimeout(changeBackground, 1000);
 
 }
 
 function draw() {
-  background(220);
+
+  if(animating == true)
+  ellipse(random(width), random(height), random(70, 350))
 
 }
 
+
+function changeBackground(){
+  if (counter <= 5) {
+    counter ++;
+  background(random(255), random(255), random(255));
+  setTimeout(changeBackground, 1000);
+  } else {
+    background(random(200,255));
+    text("it's over", 250, 300);
+  }
+}
+
+function randomizer(){
+  animating = false;
+  if (feelings[0]){
+    background(random(200, 255));
+
+    randomIndex = int(random(feelings.length));
+
+    //console.log(feelings[randomIndex].alternative);
+    text(feelings[randomIndex].alternative, 250, 300);
+    //console.log(feelings[int(random(feelings.length))].alternative);
+
+    feelings.splice(randomIndex,1);
+    //console.log(feelings);
+  } else {
+    background(random(200, 255));
+    text("Goodbye", 250, 300);
+}
+
 function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
 
-if (feelings[0]){
-  background(random(200, 255));
 
-  randomIndex = int(random(feelings.length));
-
-  //console.log(feelings[randomIndex].alternative);
-  text(feelings[randomIndex].alternative, 250, 300);
-  //console.log(feelings[int(random(feelings.length))].alternative);
-
-  feelings.splice(randomIndex,1);
-  //console.log(feelings);
-} else {
-  background(random(200, 255));
-  text("goodbye", 250, 300);
 }
 
 
