dc93097f53b3ac87a3a5ae82dffc690102690e10
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-03-04 16:19:35 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,43 @@
 //myFeelings
 let feelings = [{
-   dawn:"yee",
-   alternative: "fuck"
+  dawn: "yee",
+  alternative: "fuck"
 }, {
-   morning:"8^P",
-   alternative: "I feel so empty"
+  morning: "8^P",
+  alternative: "I feel so empty"
 }, {
-   noon:"Not sure",
-   alternative: "I don't know who I am"
+  noon: "Not sure",
+  alternative: "I don't know who I am"
 }, {
-   evening:"sad?",
-   alternative: "What's the point?"
+  evening: "sad?",
+  alternative: "What's the point?"
 }, {
-   end:"I'm tired",
-   alternative: "I'm so tired"
+  end: "I'm tired",
+  alternative: "I'm so tired"
 }];
 
 let randomIndex;
 let animating = false;
 let crew = [];
+let imageCounter = 1;
 
-function preload(){
+function preload() {
 
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     crew[i] = loadImage(`images/crew_${i}.JPG`)
-}
+  }
 
 }
 
 function setup() {
   createCanvas(600, 600);
-  background(200);
+  background(251, 201, 222);
   textSize(32);
+  textFont('Bahnschrift Light Condensed');
+  textAlign(CENTER);
+  fill(255)
+  imageMode(CENTER);
+  frameRate(6);
 
   text("Start", 250, 300);
   console.log(crew);
@@ -40,22 +46,28 @@ function setup() {
 
 function draw() {
 
-  if(animating == true)
-  ellipse(random(width), random(height), random(70, 350))
+  if (animating == true)
+    clear();
+    image(crew[imageCounter], width / 2, height / 2);
 
+  if (imageCounter < crew.length - 1) {
+    imageCounter++;
+  } else {
+    imageCounter = 0;
+  }
 }
 
 function randomizer() {
   animating = false;
-  if (feelings[0]){
-    background(random(200, 255));
-
+  if (feelings[0]) {
+    //background(random(200, 255));
+    clear();
     randomIndex = int(random(feelings.length));
-
     //console.log(feelings[randomIndex].alternative);
-    text(`${feelings[randomIndex].alternative}  What's happening?
-
-    ${feelings[randomIndex].evening}`, 50, 200);
+    image(random(crew), width / 2, height / 2);
+    text(feelings[randomIndex].alternative,
+    width/2, height - 70);
+    //${feelings[randomIndex].evening}`, 50, 200);
     //text(feelings[randomIndex].alternative + "What's happening?" +
     //feelings[randomIndex].evening, 250, 300);
     //console.log(feelings[int(random(feelings.length))].alternative);
